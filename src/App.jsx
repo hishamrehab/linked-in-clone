bdd942f2a9699409bdf64f6414955f37d5b8d1d1
hishamrehab <hishamrehab309@gmail.com>
2023-11-15 00:49:35 +0200

    first component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,35 @@
-import React from 'react'
-
+import React from "react";
+import Navbar from "./components/Navbar";
+import Rightsection from "./components/Rightsection";
+import Mainsection from "./components/Mainsection";
+import Leftsection from "./components/Leftsection";
 const App = () => {
   return (
-    <div>
- hello  react   
-    </div>
-  )
-}
+    <div className="container text-center">
+      <Navbar />
 
-export default App
+      <div className="row">
+        <div className="col-3">
+          <Leftsection />
+        </div>
+        <div className="col-6 ">
+          <Mainsection />
+        </div>
+        <div className="col-3">
+          <Rightsection />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
+
+
+
+
+
+
+
+
+
---
+++ src/components/Mainsection.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Mainsection = () => {
+  return (
+    <div>Mainsection</div>
+  )
+}
+
+export default Mainsection
